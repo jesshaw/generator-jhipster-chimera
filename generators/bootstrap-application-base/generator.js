b40b3c3266e33e97f62c7d1fc2bf0d8593cd48e2
jesshaw <jesshaw@126.com>
2024-06-04 11:27:14 +0800

    Generate the jhipster blueprint chimera

---
+++ generators/bootstrap-application-base/generator.js
@@ -0,0 +1,13 @@
+import BaseApplicationGenerator from 'generator-jhipster/generators/base-application';
+
+export default class extends BaseApplicationGenerator {
+  constructor(args, opts, features) {
+    super(args, opts, { ...features, sbsBlueprint: true });
+  }
+
+  get [BaseApplicationGenerator.PREPARING]() {
+    return this.asPreparingTaskGroup({
+      async preparingTemplateTask() {},
+    });
+  }
+}
---
+++ generators/languages/generator.js
@@ -0,0 +1,32 @@
+import BaseApplicationGenerator from 'generator-jhipster/generators/base-application';
+
+export default class extends BaseApplicationGenerator {
+  constructor(args, opts, features) {
+    super(args, opts, { ...features, sbsBlueprint: true });
+  }
+
+  get [BaseApplicationGenerator.WRITING]() {
+    return this.asWritingTaskGroup({
+      async writingTemplateTask({ application }) {
+        await this.writeFiles({
+          sections: {
+            files: [{ templates: ['template-file-languages'] }],
+          },
+          context: application,
+        });
+      },
+    });
+  }
+
+  get [BaseApplicationGenerator.WRITING_ENTITIES]() {
+    return this.asWritingEntitiesTaskGroup({
+      async writingEntitiesTemplateTask() {},
+    });
+  }
+
+  get [BaseApplicationGenerator.POST_WRITING_ENTITIES]() {
+    return this.asPostWritingEntitiesTaskGroup({
+      async postWritingEntitiesTemplateTask() {},
+    });
+  }
+}
