ed95e43699f6cbb6da017c6f7f9de6ead8485f87
jesshaw <jesshaw@126.com>
2024-06-05 15:36:20 +0800

    Added the liquibase generator

---
+++ generators/liquibase/generator.js
@@ -0,0 +1,22 @@
+import BaseApplicationGenerator from 'generator-jhipster/generators/base-application';
+
+export default class extends BaseApplicationGenerator {
+  constructor(args, opts, features) {
+    super(args, opts, { ...features, sbsBlueprint: true });
+  }
+
+  get [BaseApplicationGenerator.WRITING_ENTITIES]() {
+    return this.asWritingEntitiesTaskGroup({
+      async writingEntitiesTemplateTask() {},
+    });
+  }
+
+  get [BaseApplicationGenerator.POST_WRITING_ENTITIES]() {
+    return this.asPostWritingEntitiesTaskGroup({
+      async postWritingEntitiesTemplateTask(){},
+      // async postWritingEntitiesTemplateTask({entities}) {
+
+      // },
+    });
+  }
+}
